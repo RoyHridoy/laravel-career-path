3ca40f4c91b8a03c69dac7571be2fc738303ab62
RoyHridoy <royhridoy1428@gmail.com>
2024-06-06 11:01:28 +0600

    Position fixed for , and . on Problem 3

--- problem-03.php
+++ problem-03.php
@@ -1,7 +1,22 @@
 <?php
 
-function reverseSentenceKeepingOrder( string $sentence ): string {
-    return join( " ", array_map( fn( $word ) => strrev( $word ), explode( " ", $sentence ) ) );
+function reverse( $word ) {
+    $length = strlen( $word ) - 1;
+    for ( $i = 0, $j = $length; $i <= $j; $i++ ) {
+        if ( $word[$j] == '.' || $word[$j] == ',' ) {
+            --$j;
+        }
+        $_temp    = $word[$i];
+        $word[$i] = $word[$j];
+        $word[$j] = $_temp;
+    }
+    return $word;
 }
 
-echo reverseSentenceKeepingOrder( 'I love programming' ); // I evol gnimmargorp
+function reverseSentenceKeepingOrder( string $sentence ): string {
+    return join( " ", array_map( fn( $word ) => reverse( $word ), explode( " ", $sentence ) ) );
+}
+
+$inputString = readline( "Enter string to reverse: " );
+
+echo reverseSentenceKeepingOrder( $inputString ); // I evol, gnimmargorp.
