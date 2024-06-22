9a115b887c81554d123ebe331036ed13aefe6f07
RoyHridoy <royhridoy1428@gmail.com>
2024-06-22 13:53:21 +0600

    Initial Setup and Autoloading files

---
+++ App.php
@@ -0,0 +1,5 @@
+<?php
+
+use App\Test;
+require_once "vendor/autoload.php";
+new Test;
---
+++ src/Test.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+
+class Test
+{
+    public function __construct()
+    {
+        print "Autoloading is OK";
+    }
+}
